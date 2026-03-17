e5198dc91bc7b36eac39b90eb54295c992763e4b
James Uther <jbu@users.noreply.github.com>
2026-03-17 09:42:24 +0000

    Initial Commit

---
+++ page2markdownTests/page2markdownTests.swift
@@ -0,0 +1,17 @@
+//
+//  page2markdownTests.swift
+//  page2markdownTests
+//
+//  Created by James Uther on 17/03/2026.
+//
+
+import Testing
+@testable import page2markdown
+
+struct page2markdownTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
